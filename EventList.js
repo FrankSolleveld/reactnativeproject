79444244de791165224a5084ccb4010a91f528d7
Frank Solleveld <fsolleveld@outlook.com>
2018-12-21 12:40:08 +0100

    Added a new Component that counts down several events

--- EventList.js
+++ EventList.js
@@ -1,5 +1,14 @@
 import React, {Component} from 'react'
-import { FlatList, Text } from 'react-native'
+import { FlatList, Text, StyleSheet } from 'react-native'
+import EventCard from './EventCard'
+
+const styles = StyleSheet.create ({
+    list: {
+        flex: 1,
+        paddingTop: 40,
+        backgroundColor: '#F3F3F3'
+    }
+})
 
 class EventList extends Component {
 
@@ -8,15 +17,30 @@ class EventList extends Component {
     }
 
     componentDidMount() {
-        const events = require('./db.json').events
+
+        setInterval(() => {
+            this.setState({
+                // Events is going to be overwritten, for eacht event it should have a new attribute called timer. 
+                events: this.state.events.map(evt => ({
+                    ...evt,
+                    timer: Date.now(),
+                }))
+            })
+        }, 1000) // Updated every 1000 milliseconds
+
+        const events = require('./db.json').events.map(e => ({ // e: short for event
+            ...e, // overwriting events
+            date: new Date(e.date)
+        }))
         this.setState({ events })
     }
 
     render() {
         return (
             <FlatList 
+            style={styles.list}
             data={ this.state.events }
-            renderItem={({ item }) => <Text>{item.title}</Text>}
+            renderItem={({ item }) => <EventCard event={item} />}
             // id is unique to the item
             keyExtractor={ item => item.id }
             />
