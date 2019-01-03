ca350fb37e69664ea2b08dde8a473e1c5f353be0
Frank Solleveld <fsolleveld@outlook.com>
2019-01-03 16:19:53 +0100

    Added input fields and a datetimepicker. Created a new bug when trying to getting into EventForm

--- EventForm.js
+++ EventForm.js
@@ -2,20 +2,119 @@ import React, { Component } from 'react'
 import {
     View,
     Text,
-    TouchableHighlight
+    TouchableHighlight,
+    TextInput,
+    StyleSheet
 } from 'react-native'
+import DateTimePicker from 'react-native-modal-datetime-picker'
+import { formatDateTime } from './api'
+
+const styles = StyleSheet.create({
+    fieldContainer: {
+        marginTop: 20,
+        marginBottom: 20,
+        backgroundColor: '#FFF'
+    },
+
+    text: {
+        height: 40,
+        margin: 0,
+        marginRight: 7,
+        paddingLeft: 10
+    },
+
+    button: {
+        height:50,
+        backgroundColor: '#48BBEC',
+        borderColor: '#48BBEC',
+        borderRadius: 5,
+        alignSelf: 'stretch',
+        margin: 10,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+
+    buttonText: {
+        color: '#FFF',
+        fontSize: 18
+    },
+
+    borderTop: {
+        borderColor: '#EDEEEF',
+        borderTopWidth: 0.5
+    }
+})
 
 class EventForm extends Component {
     handleAddPress = () => {
+        console.log(this.state)
         this.props.navigation.navigate('list')
     }
 
+    handleChangeTitle = (value) => {
+         this.setState({ title: value }) 
+         console.log('handlChangeTitle executed')  
+    }
+
+    handleDatePress = () => {
+        this.setState({showDatePicker: true})
+    }
+
+    handleDatePicked = (date) => {
+        this.setState({
+            date
+        })
+
+        this.handleDatePickerHide()
+    }
+
+    handleDatePickerHide = () => {
+        this.setState({ showDatePicker: false })
+    }
+
+
     render() {
         return (
-            <View>
-                <TouchableHighlight onPress={this.handleAddPress}>
-                    <Text>Add</Text>
+            <View
+                style={{
+                    flex: 1
+                }}
+            >
+            <View style={styles.fieldContainer}>
+
+                <TextInput
+                    style={styles.text}
+                    placeholder="Event title"
+                    spellCheck={false}
+                    value={this.title}
+                    onChangeText={this.handleChangeTitle}
+                />
+
+                <TextInput      // main         override
+                    style={[styles.text, styles.borderTop]}
+                    placeholder="Event Date"
+                    spellCheck={false}
+                    value={formatDateTime(this.date.toString())}
+                    editable={!this.state.showDatePicker}
+                    onFocus={this.handleDatePress}
+                />
+                
+                <DateTimePicker 
+                    isVisible={this.state.showDatePicker}
+                    mode="datetime"
+                    onConfirm={this.handleDatePicked}
+                    onCancel={this.handleDatePickerHide}
+                />
+
+            </View>
+
+                <TouchableHighlight 
+                    onPress={this.handleAddPress}
+                    style={styles.button}
+                >
+                    <Text style={styles.buttonText}>Add</Text>
                 </TouchableHighlight>
+
             </View>
         )
     }
