9ae906bb54b6cdd2752741fc47f2b3dbcfae2bcf
Frank Solleveld <fsolleveld@outlook.com>
2019-01-03 14:46:56 +0100

    Fixed stacknavigation

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React from 'react'
-import EventList from './EventList'
+import {StyleSheet, Text, View} from 'react-native'
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <EventList />
-    )
+import EventList from './EventList'
+import { createStackNavigator, createAppContainer } from 'react-navigation'
+
+// StackNavigator is deprecated, we had to fix it this way to make it work.
+const AppNavigator = createStackNavigator({
+  Home: {
+    screen: EventList,
+    navigationOptions: () => ({
+      title: 'Your Events'
+    })
   }
-}
+})
+
+export default createAppContainer(AppNavigator)
